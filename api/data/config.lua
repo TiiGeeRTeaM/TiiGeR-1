9e65cc631a874c12e51bc134304a7d03a5fc63cc
TiGeRTeaM3 <36766246+TiGeRTeaM3@users.noreply.github.com>
2018-02-23 17:26:28 +0330

    Add files via upload

---
+++ api/data/config.lua
@@ -0,0 +1,17 @@
+do local _ = {
+  admins = {},
+  disabled_channels = {},
+  enabled_plugins = {
+    "MRCore"
+  },
+  moderation = {
+    data = "/home/tiiger/TiiGeR/cli/data/moderation.json"
+  },
+  sudo_users = {
+    205549111,
+    ,
+    
+  }
+}
+return _
+end
