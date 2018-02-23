fb25dd3ce6a107db00bf6fcb5a66495c2f6628d5
TiGeRTeaM3 <36766246+TiGeRTeaM3@users.noreply.github.com>
2018-02-23 17:38:01 +0330

    Add files via upload

---
+++ cli/data/config.lua
@@ -0,0 +1,41 @@
+do local _ = {
+  admins = {
+    {
+      205549111,
+      "@Arashwm"
+    }
+  },
+  disabled_channels = {},
+  enabled_plugins = {
+    "MRCore",
+    "Msg",
+    "BanHammer",
+    "Administrative",
+    "Help",
+    "Limitmember",
+    "Rank",
+    "Openchat",
+    "Del-Chat",
+    "Monshi",
+    "Rank",
+    "Warn",
+    "Auto-Lock",
+    "Plugins",
+    "Fuck",
+    "cleanmembers",
+    "Zarinpal",
+    "Fun",
+    "addkick"
+  },
+  moderation = {
+    data = "./data/moderation.json",
+    tabgood = "./data/tabgood.json"
+  },
+  sudo_users = {
+    205549111,
+    205549111,
+    205549111,
+  }
+}
+return _
+end
